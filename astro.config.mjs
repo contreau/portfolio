2277b565cd0ac0851bb3985753dd4f75708cce67
zenDev-2 <cfk102@gmail.com>
2023-11-19 17:49:23 -0500

    updated package.json + configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zenDev-2.github.io",
   base: "/",
-  integrations: [svelte()],
 });
