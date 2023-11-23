854ac7293e0a4f04a05d4c3a84f249118c5cefca
zenDev-2 <cfk102@gmail.com>
2023-11-22 16:20:19 -0800

    added vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zenDev-2.github.io",
   base: "/",
+  output: "server",
+  adapter: vercel()
 });
